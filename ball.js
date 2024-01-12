7d9ca7c036341dc94fc31c559cf78b55ba35b6a5
mevipinmaurya <mevipin2212001@gmail.com>
2024-01-12 20:22:29 +0530

    Adding Happy Bouncing Balls Created using javascript

---
+++ ball.js
@@ -0,0 +1,82 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+})
+
+const particlesArray = [];
+
+const mouse = {
+    x: undefined,
+    y: undefined
+}
+
+canvas.addEventListener("click", (e) => {
+    mouse.x = e.x;
+    mouse.y = e.y;
+
+    for (let i = 0; i < 5; i++) {
+        particlesArray.push(new Particles());
+    }
+    // console.log(mouse);
+})
+
+class Particles {
+    constructor() {
+        this.x = mouse.x;
+        this.y = mouse.y;
+        // this.x = canvas.width / 2;
+        // this.y = canvas.height / 2;
+        this.size = Math.random() * 20 + 1;
+        this.speedX = Math.random() * 10 - 1.5;
+        this.speedY = Math.random() * 10 - 1.5;
+    }
+
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        if (this.x + this.speedX > canvas.width - this.size || this.x + this.speedX < this.size) {
+            this.speedX = -this.speedX;
+        }
+        if (this.y + this.speedY > canvas.height - this.size || this.y + this.speedY < this.size) {
+            this.speedY = -this.speedY;
+        }
+    }
+
+    draw() {
+        ctx.beginPath();
+        // ctx.fillStyle = "rgb(138, 31, 210)";
+        ctx.fillStyle = "whitesmoke";
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+
+const handle = () => {
+    for (let i = 0; i < particlesArray.length; i++) {
+        particlesArray[i].draw();
+        particlesArray[i].update();
+    }
+}
+
+const animate = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    handle();
+    requestAnimationFrame(animate);
+}
+
+animate();
+
+
+
+// Note :
+// produce a sound whenever the balls touches the window edges
+// produce random colors for all balls 
